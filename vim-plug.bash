27b99128f10d1a78f03b1d7b4340951306f42512
Ricardo Rodriguez <rrvzero@gmail.com>
2018-07-04 09:02:10 -0400

    changes

--- vim-plug.bash
+++ vim-plug.bash
@@ -1,4 +1,103 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+cat >> ~/.vimrc << EOF
 
+" Specify a directory for plugins
+" - For Neovim: ~/.local/share/nvim/plugged
+" - Avoid using standard Vim directory names like 'plugin'
+call plug#begin('~/.vim/plugged')
+
+" Make sure you use single quotes
+
+" Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
+Plug 'junegunn/vim-easy-align'
+
+" Any valid git URL is allowed
+Plug 'https://github.com/junegunn/vim-github-dashboard.git'
+
+" Multiple Plug commands can be written in a single line using | separators
+Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
+
+" On-demand loading
+Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
+Plug 'tpope/vim-fireplace', { 'for': 'clojure' }
+
+" Using a non-master branch
+Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }
+
+" Using a tagged release; wildcard allowed (requires git 1.9.2 or above)
+Plug 'fatih/vim-go', { 'tag': '*' }
+
+" Plugin options
+Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }
+
+" Plugin outside ~/.vim/plugged with post-update hook
+Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+
+" Unmanaged plugin (manually installed and updated)
+Plug '~/my-prototype-plugin'
+
+Plug 'tpope/vim-sensible'
+
+" NERD tree will be loaded on the first invocation of NERDTreeToggle command
+Plug 'scrooloose/nerdtree', { 'on': 'NERDTreeToggle' }
+
+" Multiple commands
+Plug 'junegunn/vim-github-dashboard', { 'on': ['GHDashboard', 'GHActivity'] }
+
+" Loaded when clojure file is opened
+Plug 'tpope/vim-fireplace', { 'for': 'clojure' }
+
+" Multiple file types
+Plug 'kovisoft/paredit', { 'for': ['clojure', 'scheme'] }
+
+" On-demand loading on both conditions
+Plug 'junegunn/vader.vim',  { 'on': 'Vader', 'for': 'vader' }
+
+" Code to execute when the plugin is lazily loaded on demand
+Plug 'junegunn/goyo.vim', { 'for': 'markdown' }
+autocmd! User goyo.vim echom 'Goyo is now loaded!'
+
+" Post-update hooks
+Plug 'Shougo/vimproc.vim', { 'do': 'make' }
+Plug 'Valloric/YouCompleteMe', { 'do': './install.py' }
+" Plug 'Valloric/YouCompleteMe', { 'do': './install.py --clang-completer --cs-completer --go-completer --js-completer  --rust-completer' }
+
+
+" Custom
+Plug 'airblade/vim-gitguttern
+
+" Dev icons and ultra mega awesome fonts
+Plug 'ryanoasis/vim-devicons'
+Plug 'ryanoasis/nerd-fonts' { 'do': './install.sh' }
+
+" Git Stuff
+Plug 'Xuyuanp/nerdtree-git-plugin'
+
+Plug 'ludovicchabant/vim-gutentags'
+
+Plug 'universal-ctags/ctags'
+
+Plug 'w0rp/ale'
+Plug 'google/yapf'
+Plug 'morhetz/gruvbox'
+Plug 'tpope/vim-fugitive'
+
+Plug 'scrooloose/syntastic'
+Plug 'tpope/vim-surround'
+
+Plug 'junegunn/rainbow_parentheses.vim'
+
+" Initialize plugin system
+call plug#end()
+
+" Config for Rainbow Parentheses
+let g:rainbow#max_level = 16
+let g:rainbow#pairs = [['(', ')'], ['[', ']']]
+
+" List of colors that you do not want. ANSI code or #RRGGBB
+let g:rainbow#blacklist = [233, 234]
+
+
+EOF
--- ycm.bash
+++ ycm.bash
@@ -4,15 +4,18 @@ sudo dnf install -y ncurses-compat-libs
 #Normal installation developer suff
 sudo dnf install -y automake cmake gcc gcc-c++ kernel-devel cmake python-devel python34-devel
 sudo dnf install -y mono-devel golang rust cargo
+
 #Fix error with rpm not running as sudo
 #sudo curl -sL https://rpm.nodesource.com/setup_10.x | bash -
 wget https://rpm.nodesource.com/setup_10.x
 sed -i 's/rpm -i --nosignature/sudo rpm -i --nosignature/g' setup_10.x
 #sed -i 's/"rm -f/"sudo rm -f/g' setup_10.x
 bash setup_10.x
+rm -rf setup_10.x
 curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo yum install -y gcc-c++ make nodejs #npm
 sudo yum -y install yarn
+
 #install This as root or report error to JavaScript usign yarn
 sudo yarn global add npm@latest
 sudo yarn global add typescript
