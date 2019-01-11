a9464e83655cbc480894b11eb33bc842e09bc0e2
Ricardo Rodriguez <rrvzero@gmail.com>
2019-01-10 20:45:31 -0400

    delete no use

--- pipenv.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# https://docs.pipenv.org/basics/#general-recommendations-version-control
-
-mkdir new-project && cd new-project
-pipenv --three
-pipenv install requests
-pipenv lock
-pipenv graph
