d7f75fbd9c4fef9a0e20a70338c7451636b006b5
Ricardo Rodriguez <rrvzero@gmail.com>
2019-01-15 21:41:27 -0400

    xclip and xsel

--- f-postinstall.bash
+++ f-postinstall.bash
@@ -76,6 +76,9 @@ sudo dnf install -y chrome-gnome-shell #nome-shell-extension-dash-to-dock gir1.2
 
 #dnf install xorg-x11-drv-nouveau
 
+# install vim 8.4
+sudo dnf install -y vim xclip xsel
+
 sudo dnf install -y vim open-vm-tools sos wget mlocate net-tools which man htop wget curl lynx traceroute jwhois htop telnet openssh perl ftp deltarpm colorize bash-completion bind-utils ldns ldns-utils chrony rng-tools subnetcalc ipv6calc ipcalc gcc lshw setools-console inotify-tools tcpdump wireshark dstat sysstat ansible psmisc tree
 sudo dnf install -y nload iftop iptraf nethogs bmon bwm-ng nmap iperf iperf3
 sudo dnf groupinstall -y "Development Tools" && sudo yum install cmake -y
