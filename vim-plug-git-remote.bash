8394892ff812c9d6adc76240e040398a12a43f0e
Ricardo Rodriguez <rrvzero@gmail.com>
2019-05-18 17:43:43 -0400

    update to automate install from 0

---
+++ vim-plug-git-remote.bash
@@ -0,0 +1,9 @@
+
+wget https://raw.githubusercontent.com/Rrvz/fedora.post/master/.vimrc
+
+# old method in 2 steps
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#
+# cd ~/
+# wget https://raw.githubusercontent.com/Rrvz/fedora.post/master/.vimrc
