067d28e7bc824c31383a00b8af3abfc1978a0ff7
Ricardo Rodriguez <rrvzero@gmail.com>
2019-05-21 14:35:53 -0400

    update to wget for backups

--- vim-plug-git-remote.bash
+++ vim-plug-git-remote.bash
@@ -1,5 +1,5 @@
 
-wget https://raw.githubusercontent.com/Rrvz/fedora.post/master/.vimrc
+wget --backups https://raw.githubusercontent.com/Rrvz/fedora.post/master/.vimrc
 
 # old method in 2 steps
 # curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
